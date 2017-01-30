b581938f6e260863444d85b536f12d2546ea30b7
Daniel Lin <danlin604@gmail.com>
2017-01-29 23:06:16 -0800

    tutorial #8
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hello node!');
+
+var stuff = require('./stuff');
+
+console.log(stuff.counter(
+    ['danny', 'daniele', 'davis', 'dannyboi'])
+);
+
+console.log(stuff.adder(1,2));
+console.log(stuff.adder(1,stuff.pi));
+
---
+++ stuff.js
@@ -0,0 +1,9 @@
+module.exports.counter = function(list) {
+    return 'There are ' + list.length + ' elements in this array';
+};
+
+module.exports.adder = function(a,b) {
+    return `The sum of the ${a} + ${b} is ${a+b}`;
+};
+
+module.exports.pi = 3.142;
